f7d02d9b8acfee76308dc16f08f7e2512bf46cb8
yashkumarajora <sendto3017@gmail.com>
2021-12-20 16:41:24 -0800

    Fixed RunServer Issue

--- Modules/urls.py
+++ Modules/urls.py
@@ -1,4 +1,4 @@
-"""MyProject URL Configuration
+"""Modules URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
