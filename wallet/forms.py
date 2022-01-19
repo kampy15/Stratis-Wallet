1b816280e5c9a67c3e7901d513fbcc1c3b7f895e
yashk24489 <sendto3017@gmail.com>
2022-01-19 18:22:04 +0530

    made changes to login

--- wallet/forms.py
+++ wallet/forms.py
@@ -5,6 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 class registerform(UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2','placeholder':'Enter username'}))
+    
     LastName = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2','placeholder':'Enter LastName'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control my-2','placeholder':'Enter Email'}))
     password1=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2','placeholder':'Enter Password'}))
