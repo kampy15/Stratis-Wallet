b04a6f00816aef88285a7d5721eb4541fca797dc
divyansh <knr.guptadivyansh@gmail.com>
2022-01-26 11:18:39 +0530

    shifted main net to test net

--- wallet/views.py
+++ wallet/views.py
@@ -5,7 +5,7 @@ from matplotlib.style import context
 from pystratis.nodes import StraxNode
 from typing import List
 from django.contrib.auth.models import User
-
+from pystratis.core.networks import StraxTest
 
 
 def index(request):
@@ -18,9 +18,14 @@ def oldTxns(request):
     return render(request,'oldTxns.html')
 def profile(request):
     current_user = str(request.user)
-    node = StraxNode()
-    unused_address = node.wallet.unused_address(wallet_name=current_user) 
-    return render(request,'profile.html',{'add':[unused_address]})
+    node = StraxNode(blockchainnetwork=StraxTest())
+    unused_address = node.wallet.unused_address(wallet_name=current_user)
+    wallet_balance = node.wallet.balance(
+        wallet_name=current_user,
+        include_balance_by_address=False
+    )
+    balance=wallet_balance.balances[0].amount_confirmed
+    return render(request,'profile.html',{'add':unused_address,'bal':balance})
 def wallet(request):
     return render(request,'wallet.html')
 
@@ -38,7 +43,7 @@ def register(request):
         user.save()
 
         print("user created")
-        node = StraxNode()
+        node = StraxNode(blockchainnetwork=StraxTest())
         mnemonic: List[str] = node.wallet.create(name=username, password=pas, passphrase='') 
              
         # print(unused_address)
